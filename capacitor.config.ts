811a705da5a1e89e5b2870c8a355b83226beca57
yerali0 <astanardp@gmail.com>
2026-08-30 00:16:16 -0700

    asd

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.aligoal.app',
-  appName: 'AliGOALl',
+  appName: 'AliGoal',
   webDir: 'dist',
   plugins: {
     AdMob: {
-      appId: 'ca-app-pub-3940256099942544/5224354917',
+      // Use Google's official public test App ID (notice the tilde '~')
+      appId: 'ca-app-pub-3940256099942544~3347511713',
     },
   },
 };
